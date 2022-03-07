8a4cd5246e5056fea18506599313bf0281214af6
On0n0k1 <stiltztinkerstein@gmail.com>
2022-03-07 18:22:03 -0300

    There's no need for setting account id in this lesson

--- lesson_5_macro_usage/src/lib.rs
+++ lesson_5_macro_usage/src/lib.rs
@@ -148,53 +148,17 @@ impl Contract{
 mod tests{
     use super::*;
     use near_sdk::{
-        AccountId,
-        env,
         MockedBlockchain,
         testing_env,
         test_utils::VMContextBuilder,
-        json_types::ValidAccountId,
     };
 
     fn env_setup(){
-        let mut builder: VMContextBuilder = VMContextBuilder::new();
-
-        // attributes we can set with the builder:
-        // current_account_id
-        // signer_account_id
-        // signer_account_pk
-        // precessor_account_id
-        // block_index
-        // block_timestamp
-        // epoch_height
-        // account_balance
-        // account_locked_balance
-        // storage_usage
-        // attached_deposit
-        // prepaid_gas
-        // random_seed
-        // is_view
-
-        let account_id: AccountId = String::from("stiltztinkerstein");
-
-        builder.current_account_id(
-            ValidAccountId::try_from(
-                account_id.clone()
-            ).unwrap()
-        );
-
+        let builder: VMContextBuilder = VMContextBuilder::new();
         testing_env!(builder.build());
 
         // We're not using contract in this project. It's stateless.
         let _contract: Contract = Contract::default();
-
-        assert_eq!(
-            env::current_account_id(),
-            account_id, 
-            "Assert Error. env: {} account: {}", 
-            env::current_account_id(), 
-            &account_id,
-        );
     }
 
     #[test]
