38c8d419673f56c7411ce2e679427f63b26fdf29
On0n0k1 <stiltztinkerstein@gmail.com>
2022-06-08 09:04:38 -0300

    Finished implementing the code for lesson_6_3_game_score. Still need to finish testing + write lesson.

--- PT-BR/lesson_4_modules/src/yet_another_module/internal_module/a_deep_module.rs
+++
@@ -1,4 +0,0 @@
-/// Uma função profunda.
-pub fn a_deep_function() -> String {
-    String::from("Called a deep function")
-}
--- PT-BR/lesson_6_enums/lesson_6_2_thermometer/src/lib.rs
+++
@@ -1,8 +0,0 @@
-// Não queremos que este módulo apareça na documentação. Módulo é público apenas internamente.
-pub(crate) mod contract;
-pub mod entry;
-pub mod schedule;
-pub mod temperature;
-pub mod utils;
-
-pub use contract::Contract;
